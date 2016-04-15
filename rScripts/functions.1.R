79048687379f4d1ba8e58a1be5fabbefcd48e2b8
Umihiko Hoshijima <umihoshijima@gmail.com>
2016-04-15 09:12:52 -0700

    added functions R script

---
+++ rScripts/functions.1.R
@@ -0,0 +1,17 @@
+analyze <- function(filename) {
+  # Plots the average, min, and max inflammation over time.
+  # Input is character string of a csv file.
+  dat <- read.csv(file = filename, header = FALSE)
+  avg_day_inflammation <- apply(dat, 2, mean)
+  plot(avg_day_inflammation)
+  max_day_inflammation <- apply(dat, 2, max)
+  plot(max_day_inflammation)
+  min_day_inflammation <- apply(dat, 2, min)
+  plot(min_day_inflammation)
+}
+setwd('~/Desktop/software-carpentry-2016/data-files/r-lesson/data/')
+
+analyze("inflammation-01.csv")
+
+
+#Sys.glob()
